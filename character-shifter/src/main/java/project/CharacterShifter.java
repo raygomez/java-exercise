fc9e3a08a251ab2258d2b0769cd24e36d7fea279
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-06 08:54:32 +0800

    added CharacterShifter

---
+++ character-shifter/src/main/java/project/CharacterShifter.java
@@ -0,0 +1,29 @@
+package project;
+
+import java.io.IOException;
+import java.lang.System;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class CharacterShifter
+{
+
+    void shift(String filename) {
+        String content = "";
+        try {
+            content = new String(Files.readAllBytes(Paths.get(filename)));
+        } catch(IOException exp){
+            //System.out.prinln("Unable to read file.");
+        }
+        System.out.println(content);
+    }
+
+
+    public static void main( String[] args )
+    {
+        CharacterShifter shifter =  new CharacterShifter();
+
+        System.out.println(System.getProperty("user.dir"));
+
+    }
+}
--- character-shifter/src/test/java/project/CharacterShifterTest.java
+++ character-shifter/src/test/java/project/CharacterShifterTest.java
@@ -4,10 +4,7 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class CharacterShifterTest
     extends TestCase
 {
     /**
@@ -15,7 +12,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public CharacterShifterTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +22,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( CharacterShifterTest.class );
     }
 
     /**
