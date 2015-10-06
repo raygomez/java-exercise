1f598fef88236d68c5fddadb44b5364f05b298a1
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-07 00:58:20 +0800

    Implemented the ThreadExercise

--- thread-exercise/src/main/java/project/SleepingThread.java
+++ thread-exercise/src/main/java/project/SleepingThread.java
@@ -1,13 +1,30 @@
 package project;
 
+import static java.lang.Thread.sleep;
+
 public class SleepingThread extends Thread{
 
     int time;
-    public SleepingThread(int sleepTime){
-        time = sleepTime;
+    String name;
+
+    public SleepingThread(String name, int sleepTime){
+        this.time = sleepTime;
+        this.name = name;
     }
 
     public void run() {
-        System.out.println("Hello World!");
+
+        while(time > 0) {
+            System.out.println("Thread " + name + " is sleeping for " + time + " seconds.");
+            try {
+                Thread.sleep(time * 1000);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            time--;
+        }
+        System.out.println("Thread " + name + " is done sleeping.");
     }
 }
--- thread-exercise/src/main/java/project/ThreadExercise.java
+++ thread-exercise/src/main/java/project/ThreadExercise.java
@@ -8,7 +8,13 @@ public class ThreadExercise
 {
     public static void main( String[] args )
     {
-        SleepingThread thread1 = new SleepingThread(5);
-        thread1.start();
+        SleepingThread threadA = new SleepingThread("A", 5);
+        SleepingThread threadB = new SleepingThread("B", 7);
+        SleepingThread threadC = new SleepingThread("C", 6);
+
+        threadA.start();
+        threadB.start();
+        threadC.start();
+
     }
 }
