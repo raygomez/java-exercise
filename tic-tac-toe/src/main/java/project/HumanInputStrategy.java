7c34c1cd01af96e8b8030bca1def8b51724f6f5d
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-07 23:42:31 +0800

    moved some lines.

--- tic-tac-toe/src/main/java/project/HumanInputStrategy.java
+++ tic-tac-toe/src/main/java/project/HumanInputStrategy.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 
 public class HumanInputStrategy implements InputStrategy {
 
-    Scanner userInput = new Scanner(System.in);
-
     public Turn execute() {
 
-        Turn turn = new Turn();
+        Scanner userInput = new Scanner(System.in);
 
+        Turn turn = new Turn();
         turn.row = Integer.parseInt(userInput.next());
         turn.column = Integer.parseInt(userInput.next());
 
