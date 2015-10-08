166ffd8900c558f5e834f921ec769747ebf2bc1c
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-08 13:46:01 +0800

    do some error checking for turn input..

--- tic-tac-toe/src/main/java/inputStrategy/HumanInputStrategy.java
+++ tic-tac-toe/src/main/java/inputStrategy/HumanInputStrategy.java
@@ -19,11 +19,43 @@ public class HumanInputStrategy implements InputStrategy {
     }
 
     private int getRow(){
-        return Integer.parseInt(userInput.next());
+
+        System.out.print("Enter row:");
+        String input = userInput.nextLine();
+
+        int row;
+        while(true) {
+
+            if(input.matches("\\d")) {
+                row = Integer.parseInt(input);
+                if((row < 3) && (row > -1)){
+                    break;
+                }
+            }
+
+            System.out.print("Enter row:");
+            input = userInput.nextLine();
+        }
+        return row;
     }
 
     private int getColumn(){
-        return Integer.parseInt(userInput.next());
-    }
+        System.out.print("Enter column:");
+        String input = userInput.nextLine();
 
+        int column;
+
+        while(true) {
+            if(input.matches("\\d")) {
+                column = Integer.parseInt(input);
+                if((column < 3) && (column > -1)){
+                    break;
+                }
+            }
+
+            System.out.print("Enter column:");
+            input = userInput.nextLine();
+        }
+        return column;
+    }
 }
--- tic-tac-toe/src/main/java/project/App.java
+++ tic-tac-toe/src/main/java/project/App.java
@@ -1,5 +1,7 @@
 package project;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main( String[] args )
