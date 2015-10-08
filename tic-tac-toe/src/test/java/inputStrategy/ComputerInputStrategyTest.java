58c8544745be64f3f4406aa90830827919f654f3
Ray Gomez <rayvincent.gomez@gmail.com>
2015-10-08 14:20:21 +0800

    added test for ComputerInputStrategy

---
+++ tic-tac-toe/src/test/java/inputStrategy/ComputerInputStrategyTest.java
@@ -0,0 +1,13 @@
+package inputStrategy;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ComputerInputStrategyTest {
+
+
+    @Test
+    public void testTesting(){ }
+
+}
